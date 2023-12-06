03b8378ff024e2208c59889e86b00fdb87306bbc
jvictor12 <01joaovictordvp@gmail.com>
2023-12-06 10:38:28 -0300

    Add TokenService class, auth0 dependency and secret on application.properties

---
+++ AuthenticationAPIUsers/src/main/java/io/github/jvictor12/AuthenticationAPIUsers/service/TokenService.java
@@ -0,0 +1,57 @@
+package io.github.jvictor12.AuthenticationAPIUsers.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import io.github.jvictor12.AuthenticationAPIUsers.entity.User;
+import io.github.jvictor12.AuthenticationAPIUsers.exception.ValidationException;
+import io.github.jvictor12.AuthenticationAPIUsers.infra.security.OperationFailureException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String GenerateToken (User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
+
+            return token;
+        }catch (JWTCreationException jwtCreationException) {
+            throw new OperationFailureException("Error to create token", jwtCreationException);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException jwtVerificationException){
+            return "";
+        }
+    }
+
+    private Instant genExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
